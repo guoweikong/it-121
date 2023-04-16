050162835eaba47e60eeff0ab4b69716082621fa
guowei kong <124006367+guoweikong@users.noreply.github.com>
2023-04-16 00:15:43 +0000

    assignment

---
+++ week1_assignment/app.js
@@ -0,0 +1,6 @@
+const FirstName = "Guowei";
+const LastName = "Kong";
+const yearsOfStudy = "2 years ";
+const goal = "pursue IT as my career";
+const message = "My name is " + FirstName + " " + LastName + ". " + "I have been at SCC for " + yearsOfStudy + "and plan to " + goal + " after completing my courses.";
+console.log(message);
