07536fc2361b3f576a54cf34af5945feddd848c7
guoweikong <124006367+guoweikong@users.noreply.github.com>
2023-04-24 05:32:22 +0000

    week3 assignments

---
+++ ch3_assignment1.js
@@ -0,0 +1,6 @@
+const people = {friends:[]};
+const friend1 = {firstName: "Bren", lastName: "Rodriguez ", id: 1234};
+const friend2 = {firstName: "Jack", lastName: "Niu", id: 1235 };
+const friend3 = {firstName: "Jacquie", lastName: "George", id: 1236 };
+people.friends.push(friend1, friend2, friend3);
+console.log(people);
