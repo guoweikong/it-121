c1ce1db2eba9468535cca297547ebc45079122f4
guoweikong <124006367+guoweikong@users.noreply.github.com>
2023-04-24 05:31:52 +0000

    week3 exercises and assignments

---
+++ ch3_array1.js
@@ -0,0 +1,4 @@
+let arr = ["hi there", 5, true];
+console.log(typeof arr[0]);
+console.log(typeof arr[1]);
+console.log(typeof arr[2]);
---
+++ ch3_array4.js
@@ -0,0 +1,3 @@
+let arrOfShapes = ["circle", "triangle", "rectangle", "pentagon"];
+arrOfShapes.splice(2, 0, "square", "trapezoid");
+console.log(arrOfShapes);
